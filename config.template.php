fc9194ad33981d8355d6a632b933642502fee3d5
sheshiresat <sheshiresat@bitmessage.ch>
2017-11-01 19:13:35 -0400

    Fixed version mis-match

--- config.template.php
+++ config.template.php
@@ -55,6 +55,6 @@
   $cfg_enable_nastyhosts = true; // Whether to check with nastyhosts on the claim page.
 
   $cfg_fh_username = 'texanarcher'; // Your FaucetHUB username.
-  $cfg_site_name = 'A copy of 0xC9&#700;s Floodgate v3.0.0'; // The faucet name.
+  $cfg_site_name = 'A copy of 0xC9&#700;s Floodgate v3.1.2'; // The faucet name.
   $cfg_site_url = 'http://faucet.0xc9.net'; // The base URL of the faucet.
 ?>
