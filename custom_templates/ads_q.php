8d38afde8abd5387efbccca528aa407480e42786
japakar <39498837+japakar@users.noreply.github.com>
2019-04-07 02:17:02 -0700

    Delete ads_q.php

--- custom_templates/ads_q.php
+++
@@ -1,5 +0,0 @@
-<!-- Put some ad units here -->
-<!-- Yes, this file can be the same as ads.i.php. -->
-
-<!-- geekbasic's blkads. Just replace the BLK address in the URL with yours! -->
-<iframe sandbox="allow-forms allow-scripts" src="http://www.geekbasic.com/blkads/index.php?r=B6q6aympDZyasX5TZsxxBqmWhxtu2iNen7" style="display:block;margin-left:auto;margin-right:auto;height:90px;width:728px" scrolling="no"></iframe>
