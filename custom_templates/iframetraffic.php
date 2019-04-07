aab654978bb69a0d4d649bee6ee6ec7f18c59828
japakar <39498837+japakar@users.noreply.github.com>
2019-04-07 02:17:26 -0700

    Delete iframetraffic.php

--- custom_templates/iframetraffic.php
+++
@@ -1,5 +0,0 @@
-<!-- Perhaps someone wants to pay you for traffic? Just put their URL in an iframe here! -->
-<center>
-<iframe style="display:block;height:90px;width:720px" src="http://example.com"></iframe>
-<iframe style="display:block;height:90px;width:720px" src="http://example.com"></iframe>
-</center>
